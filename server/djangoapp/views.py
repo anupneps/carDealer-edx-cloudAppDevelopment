e71a68875af164d4ba171c0aa2091d1d137fcacc
anupneps <anup.neps@outlook.com>
2023-07-23 23:24:42 +0300

    users login logout and registration added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,23 +30,57 @@ def contact(request):
         return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
+def login_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/login.html', context)
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request=request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            messages.error(request, 'Invalid username or password.')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        return render(request, 'djangoapp/login.html', context)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    print("Log out the user `{}`".format(request.user.username))
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
-
+def registration_request(request):
+    context = {}
+    if request.method == "GET":
+        return render(request, 'djangoapp/registration.html', context)
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        
+        user_exists = User.objects.filter(username=username).exists()
+        if user_exists:
+            messages.error(request, 'Username already exists.')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        else:
+            user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+            user.save()
+            login(request, user)
+            return redirect('djangoapp:index')
+    else:
+        return render(request, 'djangoapp:registration.html', context)
+    
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
     if request.method == "GET":
         return render(request, 'djangoapp/index.html', context)
-
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 # def get_dealer_details(request, dealer_id):
@@ -56,3 +90,8 @@ def get_dealerships(request):
 # def add_review(request, dealer_id):
 # ...
 
+def index(request):
+    context = {}
+    if request.user.is_authenticated:
+        context['user'] = request.user
+    return render(request, 'djangoapp/index.html', context)
