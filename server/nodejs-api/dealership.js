6be61edc61b8ddd98dbcb3068274cc6dc85bfc53
anupneps <anup.neps@outlook.com>
2023-08-09 22:36:50 +0300

    review post api fixed, delete review added

--- server/nodejs-api/dealership.js
+++ server/nodejs-api/dealership.js
@@ -8,9 +8,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.json());
 
-console.log('CLOUDANT_API_KEY:', process.env.CLOUDANT_API_KEY);
-console.log('CLOUDANT_URL:', process.env.CLOUDANT_URL);
-
 const authenticator = new IamAuthenticator({ apikey: process.env.CLOUDANT_API_KEY });
 const cloudant = new CloudantV1({ authenticator, serviceUrl: process.env.CLOUDANT_URL, plugins: 'promises' });
 const DBNAME = 'dealerships';
@@ -23,14 +20,11 @@ app.get('/', (req, res) => {
 app.get('/dealership', async (req, res) => {
     try {
         const { state } = req.query;
-        //console.log('state:', state);
         let dealerships = [];
         const response = await cloudant.postAllDocs({ db: DBNAME, includeDocs: true });
         dealerships = response.result.rows.map(row => row.doc);
-        //console.log('dealerships:', dealerships);
 
         if(dealerships.length === 0) return res.status(404).json({ error: 'No dealerships found' });
-        
         if(state){
             const delearshipByState = dealerships.filter(dealership => {
                 dealership.state.trim().toLowerCase() === state.trim().toLowerCase()
@@ -39,9 +33,7 @@ app.get('/dealership', async (req, res) => {
             if(delearshipByState.length === 0) return res.status(404).json({ error: 'No dealerships found with the state name '+ state });   
             return res.json(delearshipByState);
         } 
-
-        res.json(dealerships);
-            
+        res.json(dealerships);    
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
@@ -71,50 +63,70 @@ app.get('/dealership/:id/review', async (req, res) => {
     try {
         const { id } = req.params;
         const response = await cloudant.postAllDocs({ db: 'reviews', includeDocs: true });
-        const filteredDealerships = response.result.rows.filter(row => row.doc.id === parseInt(id));
+        const reviews = response.result.rows.filter(row => row.doc.id.toString() === id);
 
-        if (filteredDealerships.length > 0) {
-            // Extract the 'doc' property from the filtered result
-            const dealer = filteredDealerships.map(row =>{
-                const{id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year} =row.doc;
+        if (reviews.length > 0) {
+            const dealerReview = reviews.map(row =>{
+                const{id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year} = row.doc;
                 return {id, name, dealership, review, purchase, purchase_date, car_make, car_model, car_year};
             }); 
-            
-            res.send(dealer);
+            //console.log('dealerReview:', dealerReview)
+            res.send(dealerReview);
         } else {
-            // If no dealership matches the provided id, return a 404 error
             res.status(404).json({ error: 'Dealership not found' });
         }
     } catch (error) {
-        // If any other error occurs during the database operation, return a 500 error
         res.status(500).json({ error: error.message });
     }
 });
 
 app.post('/dealership/:id/review', async (req, res) => {
-    const {id} = req.params;
-    const {review} = req.body;
-    try{
-        const response = await cloudant.postAllDocs({ db: 'reviews', includeDocs: true });
-        const existingReview = response.result.rows.find(row => row.doc.id === parseInt(id));
+    console.log('POST/Review');
+    const { id } = req.params;
+    const { review } = req.body;
 
-        if (existingReview.result) {
-            const reviewResponse = await db.postDocument({ db: 'reviews',
-            id: id,
+    try {
+        const response = await cloudant.postDocument({ 
+            db: 'reviews', 
             document: {
                 ...review,
-                _id: existingReview.result._id,
-                _rev: existingReview.result._rev, 
-            },});
-            
-            res.json(reviewResponse);
-        }
-       
+                id: id
+            },
+        });
+
+        res.send(response.result);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
 
+app.delete('/dealership/:id/review/:_id', async (req, res) => {
+    const {_id } = req.params;
+
+    try {
+        const response = await cloudant.postAllDocs({ db: 'reviews', includeDocs: true });
+
+        const reviewToDelete = response.result.rows.find(row => row.doc._id.toString() === _id);
+        console.log('reviewToDelete:', reviewToDelete);
+
+        if (!reviewToDelete) {
+            res.status(404).json({ error: 'Review not found' });
+            return;
+        }
+        console.log('deleteResponse:', reviewToDelete.doc._id);
+
+        const deleteResponse = await cloudant.deleteDocument({
+            db: 'reviews',
+            docId: reviewToDelete.doc._id,
+            rev: reviewToDelete.doc._rev
+        });
+        
+        res.json({ message: 'Review deleted successfully' });
+       
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 
 const port = process.env.PORT || 3000;
